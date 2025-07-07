863b70fc8271e7d22d9614c531bd631a44c3280d
jiwon12315 <jiwon464722@gmail.com>
2025-07-07 16:23:12 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 2.24 ms, Memory: 4.38 MB -BaekjoonHub

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.c
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.c
@@ -5,14 +5,16 @@
 int solution(int n) {
     int answer = 0;
     
-    for (int i = 2; i < n; ++i)
+    for(int i = 2; i < n; ++i)
     {
-        if (n % i == 1)
+        if(n % i == 1)
         {
             answer = i;
             break;
         }
     }
+    
+    
     
     return answer;
 }
